9c2cb7c7a622635b86bd4e1db4c3550863f76359
GhunchaNigar <awandani1434@gmail.com>
2026-08-19 18:55:23 +0500

    Fix parser reference for bulkadspost.com

--- business_extractor/dispatch.py
+++ business_extractor/dispatch.py
@@ -60,7 +60,7 @@ SITE_PARSERS = {
     "yourbizlistings.com": ("requests", parsers.yourbizlistings.parse_yourbizlistings),
     "bulkpostads.com": ("requests", parsers.bulkpostads.parse_bulkpostads),
     "meetyourmarkets.com": ("requests", parsers.meetyourmarkets.parse_meetyourmarkets),
-    "bulkadspost.com": ("requests", parsers.bulkadspost.parse_listing),
+    "bulkadspost.com": ("requests", parsers.bulkadspost.bulkadspost),
     "bizcoupon.directory": ("requests", parsers.bizcoupon.parse_bizcoupon),
     "countrypwr.com": ("requests", parsers.meetyourmarkets.parse_countrypwr),
     "homify.com": ("requests", parsers.homify.parse_homify),
